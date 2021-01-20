90c6221e3e5420a20c383f56bdc86a23620b27fd
Iman Ashoori <imanashoorii.77@gmail.com>
2021-01-20 16:23:51 +0330

    adding media url

--- cv/models.py
+++ cv/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
     university = models.CharField(max_length=100)
     experience = models.TextField()
     skills = models.TextField()
+    image = models.ImageField(upload_to='images/profile')
 
 
     def __str__(self):
