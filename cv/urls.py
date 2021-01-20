f98c437b842f71928fcbb825413629d6917221ce
Iman Ashoori <imanashoorii.77@gmail.com>
2021-01-20 17:59:25 +0330

    add pages

--- cv/urls.py
+++ cv/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'cv'
+
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('create-resume/', views.resume_form, name='create-resume'),
 ]
--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from .forms import ProfileFormItem
 # Create your views here.
 
 def landing(request):
     return render(request, 'landing.html')
+
+def resume_form(request):
+    form = ProfileFormItem(request.POST or None, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('cv:landing')
+
+    return render(request, 'add-edit-resume.html', {'form': form})
+
+
+def delete_item(request, product_id):
+    Product.objects.get(id=product_id).delete()
+    return HttpResponseRedirect("/")
